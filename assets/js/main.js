59e4ea1ef1bed766e088813d874d748bc5b8e68f
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-09 15:10:26 +0200

    cousin style html / css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,17 @@ console.log('js ok');
 // Esercizio di oggi: Slider
 // Descrizione:
 // Creare uno slider di immagini
-// Potete usare le immagini che desiderate. Per facilitarvi la vita usate immagini delle stesse dimensioni
-// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
-// In oltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
-// Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider
-// Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
-// Ricordatevi di .hasClass('my-class') per il controllo delle classe applicata a un elemento
+// Potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni
+// Lo slider prevederà due frecce (icone) che permettono di scorrere
+// tra le immagini dello slider
+// In oltre per scorrere le immagini permettere anche l’uso delle
+// frecce sinistra e destra della tastiera
+// Utiliziamo una classe first e last per capire quali sono la
+// prima e ultima immagine dello slider
+// Utilizziamo una classe active per aiutarci a capire quale è
+// l’immagine attuale da visualizzare nello slider
+// Ricordatevi di .hasClass('my-class') per il controllo delle
+// classe applicata a un elemento
 // Super importanti anche prev() e next()
 // Ricordate il $(document).ready() :wink:
